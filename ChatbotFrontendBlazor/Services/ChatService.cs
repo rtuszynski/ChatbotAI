b798d98cd090f27b431db326a14f5eaebf3ff5d8
Rafał Tuszyński <tuszynski.rafal@gmail.com>
2025-01-08 23:02:36 +0100

    Added some random response from bot

--- ChatbotFrontendBlazor/Services/ChatService.cs
+++ ChatbotFrontendBlazor/Services/ChatService.cs
@@ -19,7 +19,28 @@ public class ChatService
 
     public async Task AddMessageAsync(ChatMessage message)
     {
+        message.BotResponse = GenerateBotResponse();
         await _httpClient.PostAsJsonAsync("chat/messages", message);
+    }
+
+    private string GenerateBotResponse()
+    {
+        var random = new Random();
+        int responseType = random.Next(3);
+
+        switch (responseType)
+        {
+            case 0:
+                return "To jest krótka odpowiedź bota.";
+            case 1:
+                return "To jest średnia odpowiedź bota, która zawiera więcej informacji i składa się z kilku zdań.";
+            case 2:
+                return "To jest bardzo długa odpowiedź bota, która składa się z kilku akapitów. Może zawierać " +
+                    "wiele szczegółów i informacji, które są istotne dla użytkownika. Taka odpowiedź może być " +
+                    "użyteczna w przypadku bardziej skomplikowanych zapytań, które wymagają szczegółowego wyjaśnienia.";
+            default:
+                return "To jest domyślna odpowiedź bota.";
+        }
     }
 
     public async Task UpdateRatingAsync(int id, int rating)

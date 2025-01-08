e475ce029d580e6ff441abe94d42a467e67d4396
Rafał Tuszyński <tuszynski.rafal@gmail.com>
2025-01-09 00:05:30 +0100

    Added simple validation for chat input

--- ChatbotFrontendBlazor/Models/ChatMessage.cs
+++ ChatbotFrontendBlazor/Models/ChatMessage.cs
@@ -1,8 +1,13 @@
-﻿namespace ChatbotFrontendBlazor.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChatbotFrontendBlazor.Models;
 
 public class ChatMessage
 {
     public int Id { get; set; }
+
+    [Required(ErrorMessage = "Wiadomość nie może być pusta")]
+    [StringLength(100, ErrorMessage = "Wiadomość nie może być dłuższa niż 100 znaków")]
     public string UserMessage { get; set; }
     public string BotResponse { get; set; }
     public int? Rating { get; set; }

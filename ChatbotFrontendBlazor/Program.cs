097db91cdb581420d793ea52a933554796c97cd6
Rafał Tuszyński <tuszynski.rafal@gmail.com>
2025-01-09 09:51:46 +0100

    Visual fixes

--- ChatbotFrontendBlazor/Program.cs
+++ ChatbotFrontendBlazor/Program.cs
@@ -2,20 +2,19 @@ using ChatbotFrontendBlazor.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-namespace ChatbotFrontendBlazor
+namespace ChatbotFrontendBlazor;
+
+public class Program
 {
-    public class Program
+    public static async Task Main(string[] args)
     {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
-            builder.RootComponents.Add<HeadOutlet>("head::after");
-            builder.Services.AddScoped<ChatService>();
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        builder.RootComponents.Add<App>("#app");
+        builder.RootComponents.Add<HeadOutlet>("head::after");
+        builder.Services.AddScoped<ChatService>();
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5189/api/") });
+        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5189/api/") });
 
-            await builder.Build().RunAsync();
-        }
+        await builder.Build().RunAsync();
     }
 }
